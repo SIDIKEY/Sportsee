e85e37a97ee15794cabc1b57a0897244ccf3c838
SIDIKEY <92093281+SIDIKEY@users.noreply.github.com>
2024-11-10 11:25:59 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,139 +11,157 @@ import proteinWidget from "./Assets/protein-icon.svg";
 import carbsWidget from "./Assets/carbs-icon.svg";
 import fatWidget from "./Assets/fat-icon.svg";
 
-
-import './App.css';
+import "./App.css";
 
 function App() {
+  function getCurrentURL() {
+    return window.location.href;
+  }
 
-  const url = getCurrentURL();
+  let url = getCurrentURL();
+  
+  
   let id = url.split(`3001/`).pop();
+    
+  
+  
+
+  function findID(id) {
+    if(!id || id=="") {
+      
+      let alerted = localStorage.getItem('alerted') || '';
+      if (alerted != 'yes') {
+        window.alert("please put your user id in the url like this: `http://localhost:3001/12`")
+        console.log("no user id");
+      localStorage.setItem('alerted','yes');
+    }
+      
+      
+      
+     //. window.location.href += "12"
+    }
+
+    
+  }
+
+  findID()
+
+  
+
+
+  
+
+  
 
   const [data, loading] = useData(id);
 
-  function getCurrentURL() {
-    return window.location.href;
-  } 
+ 
+  console.log(data);
 
-  console.log(data)
-  
   return (
     <div>
       <Header />
       <Sidebar />
-      
-      <div className="dashboard">
 
+      <div className="dashboard">
         <div className="Title">
           <Title
             title="Bonjour"
-            username={loading ? "loading" : data ?.user?.userInfos.firstName || "" }
+            username={
+              loading ? "loading" : data?.user?.userInfos.firstName || ""
+            }
           />
-            <h2>Félicitations vous avez explosé vos objectifs hier 👏</h2>
+          <h2>Félicitations vous avez explosé vos objectifs hier 👏</h2>
         </div>
 
         <div className="mainWrapper">
-
-
-        
-
-        <div className="main">
+          <div className="main">
             <div>
               <Simplebarchart data={data?.activity?.sessions || ""} />
             </div>
 
             <div className="charts_wrapper">
               <ChartLine data={data?.averageSessions?.sessions || ""} />
-              
-              <RadChart dataPerf={data? data.performance : ""}/>
 
-              <ChartScore dataScore={Number(data?.user?.todayScore) || data?.user?.score || 0}/>
-            </div>
-        </div>
+              <RadChart dataPerf={data ? data.performance : ""} />
 
-
-
-          
-
-         <aside>
-          <div className="widgets_wrapper">
-            <div className="calories_widget">
-              <div className="cal_widget">
-                <img
-                  src={caloriesWidget}
-                  alt="calorie icône"
-                  className="c_widget"
-                />
-              </div>
-              <div>
-                <p className="widget_value">{data?.user?.keyData?.calorieCount + "kcal" || ""}</p>
-                <p className="widget_unit">Calories</p>
-              </div>
-            </div>
-            <div className="calories_widget">
-              <div className="cal_widget">
-                <img
-                  src={proteinWidget}
-                  alt="proteines icône"
-                  className="c_widget"
-                />
-              </div>
-              <div>
-                <p className="widget_value">{data?.user?.keyData?.proteinCount + "g" || ""}</p>
-                <p className="widget_unit">Protéines</p>
-              </div>
-            </div>
-            <div className="calories_widget">
-              <div className="cal_widget">
-                <img
-                  src={carbsWidget}
-                  alt="gludides icône"
-                  className="c_widget"
-                />
-              </div>
-              <div>
-                <p className="widget_value">{data?.user?.keyData?.carbohydrateCount + "g" || ""}</p>
-                <p className="widget_unit">Glucides</p>
-              </div>
-            </div>
-            <div className="calories_widget">
-              <div className="cal_widget">
-                <img
-                  src={fatWidget}
-                  alt="lipides icône"
-                  className="c_widget"
-                />
-              </div>
-              <div>
-                <p className="widget_value">{data?.user?.keyData?.lipidCount + "g" || ""}</p>
-                <p className="widget_unit">lipides</p>
-              </div>
+              <ChartScore
+                dataScore={
+                  Number(data?.user?.todayScore) || data?.user?.score || 0
+                }
+              />
             </div>
           </div>
-        </aside>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+          <aside>
+            <div className="widgets_wrapper">
+              <div className="calories_widget">
+                <div className="cal_widget">
+                  <img
+                    src={caloriesWidget}
+                    alt="calorie icône"
+                    className="c_widget"
+                  />
+                </div>
+                <div>
+                  <p className="widget_value">
+                    {data?.user?.keyData?.calorieCount + "kcal" || ""}
+                  </p>
+                  <p className="widget_unit">Calories</p>
+                </div>
+              </div>
+              <div className="calories_widget">
+                <div className="cal_widget">
+                  <img
+                    src={proteinWidget}
+                    alt="proteines icône"
+                    className="c_widget"
+                  />
+                </div>
+                <div>
+                  <p className="widget_value">
+                    {data?.user?.keyData?.proteinCount + "g" || ""}
+                  </p>
+                  <p className="widget_unit">Protéines</p>
+                </div>
+              </div>
+              <div className="calories_widget">
+                <div className="cal_widget">
+                  <img
+                    src={carbsWidget}
+                    alt="gludides icône"
+                    className="c_widget"
+                  />
+                </div>
+                <div>
+                  <p className="widget_value">
+                    {data?.user?.keyData?.carbohydrateCount + "g" || ""}
+                  </p>
+                  <p className="widget_unit">Glucides</p>
+                </div>
+              </div>
+              <div className="calories_widget">
+                <div className="cal_widget">
+                  <img
+                    src={fatWidget}
+                    alt="lipides icône"
+                    className="c_widget"
+                  />
+                </div>
+                <div>
+                  <p className="widget_value">
+                    {data?.user?.keyData?.lipidCount + "g" || ""}
+                  </p>
+                  <p className="widget_unit">lipides</p>
+                </div>
+              </div>
+            </div>
+          </aside>
         </div>
-
-        
-
-       
       </div>
     </div>
   );
 }
+
 
 export default App;
