8fb143835d0291bc556fc42a1357f59ddbe9f373
SIDIKEY <92093281+SIDIKEY@users.noreply.github.com>
2024-11-14 00:56:38 +0100

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
+import { useState, useEffect } from "react";
 import logo from "./logo.svg";
-import "./Header.css"
+import "./Header.css";
 
-function Header() {
-    return (
-            <nav className='nav_bar'>
-                <div className='logo_sportsee'>
-                    <img alt="logo sportsee" src={logo} />
-                </div>
-				<ul className="nav_links">
-					<li className="link">Accueil</li>
-					<li className="link">Profil</li>
-					<li className="link">Réglage</li>
-					<li className="link">Communauté</li>
-				</ul>
-			</nav>
-      
-    );
+function Header({ onToggleResource, resource }) {
+  return (
+    <nav className="nav_bar">
+      <div className="logo_sportsee">
+        <img alt="logo sportsee" src={logo} />
+      </div>
+      <ul className="nav_links">
+        <li className="link">Accueil</li>
+        <li className="link">Profil</li>
+        <li className="link">Réglage</li>
+        <li className="link">Communauté</li>
+      </ul>
+      <button onClick={onToggleResource}>
+        Use {resource === "mock" ? "api" : "mock"}
+      </button>
+    </nav>
+  );
 }
-  
+
 export default Header;
