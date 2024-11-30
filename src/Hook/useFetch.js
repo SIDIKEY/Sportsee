d81360389157abb49ff76bead2e16415b85d5c0c
SIDIKEY <92093281+SIDIKEY@users.noreply.github.com>
2024-11-30 09:27:58 +0100

    Update useFetch.js

--- src/Hook/useFetch.js
+++ src/Hook/useFetch.js
@@ -1,61 +1,57 @@
 import { useState, useEffect } from "react";
 
-export const useFetch = (urlMock) => {
-    const [tData, setData] = useState(null);
-    useEffect(() => {
-      const fetchData = async () => {
-        const response = await fetch(`${urlMock}/data.json`
-            ,{
-              headers : { 
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-               }
-            }
-            );
+export const useFetch = (urlMock, id) => {
+  const [tData, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      setError(false);
+
+      try {
+        const response = await fetch(`${urlMock}/data.json`, {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+        });
+
         const json = await response.json();
-        
+        setIsPending(true);
+
         let tData = [];
         function getCurrentURL() {
           return window.location.href;
-        };
+        }
 
         let url = getCurrentURL();
         let id = url.split(`3001/`).pop();
-        let result = json.find(user => user.id == id) ;
-        
-         
+
+        let result = json.find((user) => user.id == id);
 
         let averageSessions = () => result.sessionsLength;
         let activity = () => result.sessions;
-        let performance = () => result.data;
+        let performance = () => result.performance;
 
-       let user = () => Object.keys(tData.userInfos).forEach(function(key, index) {
-        return { user : tData.userInfos }
-      });
-
-      let info = result;
-      
-
-      const target = { info };
-      const source = { info : user};
-
-      const returnedTarget = Object.assign(target, source);
-
-        
         let key = "averageSessions";
         let newKey = "activity";
         let brandNewKey = "performance";
-        let lastKey = "user"
+        let lastKey = "user";
         tData = result;
-        tData[key] = {sessions: averageSessions()}
-        tData[newKey] = {sessions: activity()}
-        tData[brandNewKey] = {sessions: performance()}
-        tData[lastKey] = result
-       
+        tData[key] = { sessions: averageSessions() };
+        tData[newKey] = { sessions: activity() };
+        tData[brandNewKey] = performance();
+        tData[lastKey] = result;
+
         tData = result;
         setData(tData);
-      };
-      fetchData();
-    }, [urlMock]);
-    return { tData };
-  };
+        setIsPending(false);
+      } catch (error) {
+        setError(true);
+        setIsPending(false);
+      }
+    };
+    fetchData();
+  }, [urlMock]);
+  return { tData, isPending, error };
+};
